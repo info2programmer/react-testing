200ae89aed79b9c1ebc67de80f267b71cf71db0c
Saikat Bhadury <info2programmer@gmail.com>
2025-04-30 15:44:54 +0530

    refactor(App): simplify component structure and update input handling

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,76 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import { describe, test, expect } from 'vitest';
-import App from './App';
-
-describe.skip('Group App component', () => {
-    test('renders first react test case', () => {
-        render(<App />);
-        const testCaseElement = screen.getByText(/First react test case/i);
-        const testCaseElement2 = screen.getByText(/Saikat Bhadury/i);
-        const title = screen.getByTitle(/logo react/i);
-        expect(title).toBeDefined();
-        expect(testCaseElement).toBeDefined();
-        expect(testCaseElement2).toBeDefined();
-    });
+import { test, expect } from "vitest";
+import { fireEvent, render, screen } from '@testing-library/react';
+import App from "./App";
 
 
-    test('testing input box', () => {
-        render(<App />);
-        const checkInput = screen.getByRole('textbox');
-        expect(checkInput).toBeDefined();
-        expect(checkInput).toHaveProperty('type', 'text');
-        expect(checkInput).toHaveProperty('placeholder', 'enter username');
-        expect(checkInput).toHaveProperty('name', 'txtUserName');
-        expect(checkInput).toHaveProperty('value', 'Saikat');
-        // expect(checkInput).toHaveAttribute('placeholder', 'enter username');
-    });
-
-})
-
-
-describe('Test Case APP API', () => {
-    test('renders first react test case', () => {
-        render(<App />);
-        const testCaseElement = screen.getByText(/First react test case/i);
-        const testCaseElement2 = screen.getByText(/Saikat Bhadury/i);
-        const title = screen.getByTitle(/logo react/i);
-        expect(title).toBeDefined();
-        expect(testCaseElement).toBeDefined();
-        expect(testCaseElement2).toBeDefined();
-    });
-
-
-
-
-    describe("Inner Test", () => {
-        test('testing input box', () => {
-            render(<App />);
-            const checkInput = screen.getByRole('textbox');
-            expect(checkInput).toBeDefined();
-            expect(checkInput).toHaveProperty('type', 'text');
-            expect(checkInput).toHaveProperty('placeholder', 'enter username');
-            expect(checkInput).toHaveProperty('name', 'txtUserName');
-            expect(checkInput).toHaveProperty('value', 'Saikat');
-            // expect(checkInput).toHaveAttribute('placeholder', 'enter username');
-        });
-    })
-
-})
-
-
-
-// describe('App component', () => {
-//     test('renders Vite + React heading', () => {
-//         // Render the App component
-//         render(<App />);
-
-//         // Find the heading element by text content
-//         // The 'i' flag makes the match case-insensitive
-//         const headingElement = screen.getByRole('heading', {
-//             name: /vite \+ react/i,
-//         });
-
-//         // Assert that the heading element is in the document
-//         expect(headingElement).toBeDefined();
-//     });
-// });
+test("on change event testing", () => {
+    render(<App />);
+    const input = screen.getByRole("textbox") as HTMLInputElement;
+    fireEvent.change(input, { target: { value: "a" } });
+    expect(input.value).toBe("atest");
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,17 @@
-// import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
+
+
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [data, setdata] = useState<string>("")
 
   return (
-    <>
-      {/* <div>
-         <a href="https://vite.dev" target="_blank" rel="noopener">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
 
-      <div>
-        <p>First react test Case</p>
-        <p>Saikat Bhadury</p>
-        <input type="text" name="txtUserName" id="" placeholder='enter username' readOnly value={"Saikat"} />
-        <a href="https://react.dev" target="_blank" rel="noopener">
-          <img src={reactLogo} className="logo react" alt="React logo" title="logo React" />
-        </a>
-      </div>
+    <>
+      <h1>Test OnChange Event With Input Text</h1>
+      <input type="text" value={data} placeholder='enter username' onChange={(e) => setdata(e.target.value + "test")} />
     </>
   )
 }
