b53c78387fb42807925895356de868bc4551a1e3
Saikat Bhadury <info2programmer@gmail.com>
2025-04-30 19:12:02 +0530

    test: restore click event test for App component

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import { test, expect } from "vitest";
 import { fireEvent, render, screen } from '@testing-library/react';
-import App from "./App";
+import App from "../App";
 import '@testing-library/jest-dom';
 
 
